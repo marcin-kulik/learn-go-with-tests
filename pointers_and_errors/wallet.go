d28e4342b3405f095bd5e3b5075bccf5d7cde418
Marcin Kulik <marcinkulik@icloud.com>
2020-10-11 20:52:28 +0100

    instead of copying a wallet, we point to a wallet - test is passing

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -6,12 +6,12 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	fmt.Printf("address of balance in Balance is %v \n", &w.balance)
 	return w.balance
 }
