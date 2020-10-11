182367327479e8bde8422fa37600424fb3ec6bcf
Marcin Kulik <marcinkulik@icloud.com>
2020-10-11 20:47:09 +0100

    in Go when we call a method arguments are copied; printing different addresses

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -1,13 +1,17 @@
 package pointers_and_errors
 
+import "fmt"
+
 type Wallet struct {
 	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
+	fmt.Printf("address of balance in Deposit is %v \n", &w.balance)
 	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
+	fmt.Printf("address of balance in Balance is %v \n", &w.balance)
 	return w.balance
 }
--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -1,13 +1,17 @@
 package pointers_and_errors
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-
 	wallet.Deposit(10)
 
 	got := wallet.Balance()
+	fmt.Printf("address of balance in test is %v \n", &wallet.balance)
+
 	want := 10
 
 	if got != want {
